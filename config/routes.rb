911d91a18333b38624c02b5a3863e3a1b136cb5f
Kyle Lortscher <kyle91302@gmail.com>
2015-04-20 12:35:51 -0700

    Added Product Listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :listings
+
   get 'pages/about'
 
   get 'pages/contact'
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'listings#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
