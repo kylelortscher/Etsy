f2a6ee6ad99ffa21004bb9493ed24ea69599142a
Kyle Lortscher <kyle91302@gmail.com>
2015-04-20 17:25:50 -0700

    Added Validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,9 @@
 class Listing < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
+    validates :name, :descripition, :price, presence: true
+    validates :price, numericality: { greater_than: 0}
+    validates_attacment_presence :image
 end
